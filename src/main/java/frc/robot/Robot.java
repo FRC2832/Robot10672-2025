02d076333566c7a60549941253992ec520df2f84
Michael Gutterman <programmer94@users.noreply.github.com>
2025-03-21 15:33:49 -0400

    Update WPILib. Improve drive control. Improve auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -73,6 +73,7 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopPeriodic() {
         double coralSpeed = 0.0; // Default to no movement
+        double driveSpeedFactor = 0.75;
         // TODO: Maybe add a drive speed multiplier variable?
         // Get the buttons from a Joystick or XboxController and use them
         // to control the coral mechanism.
@@ -93,8 +94,12 @@ public class Robot extends TimedRobot {
 
         // Set the motor outputs. Invert the Y axis so that forward is positive and
         // backward is negative.
-        double leftSpeed = -driverController.getLeftY() * 0.6;
-        double rightSpeed = -driverController.getRightY() * 0.6;
+        if (driverController.getLeftTriggerAxis() > 0.5) {
+            driveSpeedFactor = 0.5;
+        }
+
+        double leftSpeed = -driverController.getLeftY() * driveSpeedFactor;
+        double rightSpeed = -driverController.getRightY() * driveSpeedFactor;
         robotDrive.tankDrive(leftSpeed, rightSpeed, true);
         coralMotor.set(coralSpeed);
     }
@@ -136,22 +141,33 @@ public class Robot extends TimedRobot {
             case 1:
                 // Turn right for 1 second.
                 coralMotor.set(0.0);
-                robotDrive.tankDrive(0.5, -0.5);
+                robotDrive.tankDrive(-0.5, 0.5);
                 if (ROBOT_TIMER.get() > 3.0) {
                     robotDrive.tankDrive(0.0, 0.0);
                     if (chosenAuton == 1) {
                         // If the user chose the move-only auton routine, we're done.
-                        autonStep = 3;
+                        autonStep = 4;
                     } else {
                         autonStep++;
                     }
                 }
                 break;
             case 2:
+                // Start the timer if it hasn't been started yet.
+                ROBOT_TIMER.start();
+                // Drive forward for 2.5 seconds.
+                coralMotor.set(0.0);
+                robotDrive.tankDrive(0.5, 0.5);
+                if (ROBOT_TIMER.get() > 5.5) {
+                    robotDrive.tankDrive(0.0, 0.0);
+                    autonStep++;
+                }
+                break;
+            case 3:
                 // Run coral mechanism for 2 seconds.
                 coralMotor.set(0.5);
                 robotDrive.tankDrive(0.0, 0.0);
-                if (ROBOT_TIMER.get() > 5.0) {
+                if (ROBOT_TIMER.get() > 7.5) {
                     coralMotor.set(0.0);
                     autonStep++;
                 }
